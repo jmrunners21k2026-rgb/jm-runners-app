375ad62f54bb7757928a17135f830724357a7e92
jmrunners21k2026-rgb <jm.runners.21k.2026@gmail.com>
2026-02-08 18:58:17 -0300

    CORREÇÃO NETLIFY

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: './', // Essencial para o GitHub Pages
   server: {
     port: 3000,
     open: true
